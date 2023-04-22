4591e0836364adc833ca9cb80d1a83d762c2166c
RomanRVV <rvachyov.roma@yandex.ru>
2023-04-22 20:27:42 +0300

    created separate scripts from a main.py

---
+++ work_with_pic.py
@@ -0,0 +1,22 @@
+import requests
+from pathlib import Path
+from os.path import split, splitext
+from urllib.parse import urlsplit, unquote
+
+
+def download_pic(url, filename):
+    Path('images/').mkdir(parents=True, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(f'images/{filename}', 'wb') as file:
+        file.write(response.content)
+    return
+
+
+def find_file_ext(url):
+    url_tuple = urlsplit(url)
+    path = unquote(url_tuple[2])
+    file_name = split(path)[1]
+    file_ext = splitext(file_name)[1]
+    return file_ext
