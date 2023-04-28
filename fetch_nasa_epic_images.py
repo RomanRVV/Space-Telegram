8e0bd88d70e855fded8e94aac4955534d4d55c1b
RomanRVV <rvachyov.roma@yandex.ru>
2023-04-28 18:00:39 +0300

    add error protection in get parameters

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -7,24 +7,25 @@ from work_with_pic import download_pic
 
 
 def fetch_epic_pic(api_key, number_of_pic):
-    pic_info_url = f'https://api.nasa.gov/EPIC/api/natural/available?api_key={api_key}'
-    response = requests.get(pic_info_url)
+    payload = {'api_key': api_key}
+    pic_info_url = f'https://api.nasa.gov/EPIC/api/natural/available'
+    response = requests.get(pic_info_url, params=payload)
     response.raise_for_status()
 
     pic_info = response.json()
     for count, pic in enumerate(pic_info[(-number_of_pic-1):-1]):
         aDate = datetime.date.fromisoformat(pic)
         formatted_date = aDate.strftime('%Y/%m/%d')
-        url = f"https://api.nasa.gov/EPIC/api/natural/date/{aDate}?api_key={api_key}"
-        response = requests.get(url)
+        url = f"https://api.nasa.gov/EPIC/api/natural/date/{aDate}"
+        response = requests.get(url, params=payload)
         response.raise_for_status()
         try:
             download_pic_url = f'https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/' \
-                               f'{response.json()[0]["image"]}.png?api_key={api_key}'
-            response = requests.get(download_pic_url)
+                               f'{response.json()[0]["image"]}.png'
+            response = requests.get(download_pic_url, params=payload)
             response.raise_for_status()
             filename = f'nasa_EPIC_{count}.png'
-            download_pic(download_pic_url, filename)
+            download_pic(response.url, filename)
         except IndexError:
             print('В сервисе EPIC отсутствует картинка на дату:', formatted_date)
 
