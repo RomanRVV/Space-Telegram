5355ae7dfad34773aba19deb0acb42ed70eb4d16
RomanRVV <rvachyov.roma@yandex.ru>
2023-04-28 20:40:41 +0300

    split functions into parts

--- fetch_nasa_apod_images.py
+++ fetch_nasa_apod_images.py
@@ -5,16 +5,15 @@ from dotenv import load_dotenv, find_dotenv
 from work_with_pic import download_pic, find_file_ext
 
 
-def fetch_apod_pic(api_key, number_of_pic):
-    payload = {'api_key': api_key,
-               'count': number_of_pic,
-               'thumbs': True
-               }
-
+def create_apod_pic_list(payload):
     url = 'https://api.nasa.gov/planetary/apod'
     response = requests.get(url, params=payload)
     response.raise_for_status()
     apod_pic = response.json()
+    return apod_pic
+
+
+def fetch_apod_pic(apod_pic):
     pic_list = []
     for pic in apod_pic:
         if pic['url']:
@@ -40,8 +39,13 @@ def main():
     parser.add_argument('number_of_pic', help='Сколько картинок нужно скачать')
     args = parser.parse_args()
     nasa_api_key = os.environ['NASA_API_KEY']
+    payload = {'api_key': nasa_api_key,
+               'count': int(args.number_of_pic),
+               'thumbs': True
+               }
+    apod_pic = create_apod_pic_list(payload)
     try:
-        fetch_apod_pic(nasa_api_key, int(args.number_of_pic))
+        fetch_apod_pic(apod_pic)
         print('Все фото скачены')
     except ValueError:
         print('Вы ввели не число')
--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -6,32 +6,32 @@ from dotenv import load_dotenv, find_dotenv
 from work_with_pic import download_pic
 
 
-def fetch_epic_pic(api_key, number_of_pic):
-    payload = {'api_key': api_key}
+def create_pic_info_list(payload):
     pic_info_url = f'https://api.nasa.gov/EPIC/api/natural/available'
     response = requests.get(pic_info_url, params=payload)
     response.raise_for_status()
-
     pic_info = response.json()
-    for count, pic in enumerate(pic_info[(-number_of_pic-1):-1]):
-        a_date = datetime.date.fromisoformat(pic)
-        formatted_date = a_date.strftime('%Y/%m/%d')
-        url = f"https://api.nasa.gov/EPIC/api/natural/date/{a_date}"
-        response = requests.get(url, params=payload)
+    return pic_info
+
+
+def fetch_epic_pic(payload, pic, pic_number):
+    a_date = datetime.date.fromisoformat(pic)
+    formatted_date = a_date.strftime('%Y/%m/%d')
+    url = f"https://api.nasa.gov/EPIC/api/natural/date/{a_date}"
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    pic_url_for_download = f'https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/' \
+                           f'{response.json()[0]["image"]}.png'
+    filename = f'nasa_EPIC_{pic_number}.png'
+    response = requests.get(pic_url_for_download, params=payload)
+    try:
         response.raise_for_status()
-        pic_url_for_download = f'https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/' \
-                               f'{response.json()[0]["image"]}.png'
-        filename = f'nasa_EPIC_{count}.png'
-        response = requests.get(pic_url_for_download, params=payload)
-        try:
-            response.raise_for_status()
-            download_pic(response.url, filename)
-        except IndexError:
-            print('В сервисе EPIC отсутствует картинка на дату:', formatted_date)
+        download_pic(response.url, filename)
+    except IndexError:
+        print('В сервисе EPIC отсутствует картинка на дату:', formatted_date)
 
 
 def main():
-
     load_dotenv(find_dotenv())
     parser = argparse.ArgumentParser(
         description='Скачивает картинки с сервиса NASA EPIC'
@@ -39,11 +39,15 @@ def main():
     parser.add_argument('number_of_pic', help='Сколько картинок нужно скачать')
     args = parser.parse_args()
     nasa_api_key = os.environ['NASA_API_KEY']
-    try:
-        fetch_epic_pic(nasa_api_key, int(args.number_of_pic))
-        print('Все фото скачены')
-    except ValueError:
-        print('Вы ввели не число')
+
+    payload = {'api_key': nasa_api_key}
+    pic_info = create_pic_info_list(payload)
+    for count, pic in enumerate(pic_info[(-int(args.number_of_pic) - 1):-1]):
+        try:
+            fetch_epic_pic(payload, pic, count)
+            print('Все фото скачены')
+        except ValueError:
+            print('Вы ввели не число')
 
 
 if __name__ == '__main__':
